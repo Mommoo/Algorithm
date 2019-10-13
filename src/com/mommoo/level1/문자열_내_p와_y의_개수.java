8650592f69527c7955499c02b79d5e927dedbd96
Mommoo <dv.mommoo@gmail.com>
2019-10-13 22:49:16 +0900

    ## level1 두 정수 사이의 합 ## level1 문자열 내 p와 y의 개수 ## level1 문자열 내 마음대로 정렬하기 ## level2 문자열 내림차순으로 배치하기

---
+++ src/com/mommoo/level1/문자열_내_p와_y의_개수.java
@@ -0,0 +1,25 @@
+package com.mommoo.level1;
+
+public class 문자열_내_p와_y의_개수 {
+    public static void main(String[] args) {
+        System.out.println(new 문자열_내_p와_y의_개수().solution("pPoooyY"));
+        System.out.println(new 문자열_내_p와_y의_개수().solution("Pyy"));
+    }
+
+    boolean solution(String s) {
+        int count = 0;
+
+        for (int i = 0; i < s.length() ; i++) {
+            char c = s.charAt(i);
+            char lowercaseC = Character.toLowerCase(c);
+
+            if (lowercaseC == 'p') {
+                count ++;
+            } else if (lowercaseC == 'y') {
+                count --;
+            }
+        }
+
+        return count == 0;
+    }
+}
