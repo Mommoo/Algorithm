0ef523337a6239e18ab7c22546c9d98a9ae3dd71
Mommoo <dv.mommoo@gmail.com>
2019-10-16 03:06:02 +0900

    ## level1 예산

---
+++ src/com/mommoo/level1/예산.java
@@ -0,0 +1,24 @@
+package com.mommoo.level1;
+
+import java.util.Arrays;
+
+public class 예산 {
+    public static void main(String[] args) {
+        System.out.println(new 예산().solution(new int[]{1, 3, 2, 5, 4}, 9));
+        System.out.println(new 예산().solution(new int[]{2, 2, 3, 3}, 10));
+    }
+
+    public int solution(int[] d, int budget) {
+        Arrays.sort(d);
+        int num = 0;
+        for (int value : d) {
+            if (value > budget) {
+                break;
+            }
+
+            budget -= value;
+            num++;
+        }
+        return num;
+    }
+}
