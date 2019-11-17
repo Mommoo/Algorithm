7269723029ec289c4819e38c37d20170cf2410e6
Mommoo <dv.Mommoo@gmail.com>
2019-11-17 16:43:42 +0900

    level2: 더 맵게

---
+++ src/com/mommoo/level2/더_맵게.java
@@ -0,0 +1,30 @@
+package com.mommoo.level2;
+
+import java.util.Arrays;
+import java.util.PriorityQueue;
+import java.util.stream.Collectors;
+
+public class 더_맵게 {
+    public static void main(String[] args) {
+        System.out.println(new 더_맵게().solution(new int[]{1, 2, 3, 9, 10, 12}, 7));
+    }
+
+    public int solution(int[] scoville, int K) {
+        PriorityQueue<Integer> priorityQueue = Arrays.stream(scoville)
+                                                     .boxed()
+                                                     .collect(Collectors.toCollection(PriorityQueue::new));
+
+        int answer = 0;
+        while (priorityQueue.size() >= 2 && priorityQueue.peek() < K) {
+            int newK = priorityQueue.poll() + priorityQueue.poll() * 2;
+            priorityQueue.add(newK);
+            answer++;
+        }
+
+        if (priorityQueue.isEmpty() || priorityQueue.peek() < K) {
+            return -1;
+        }
+
+        return answer;
+    }
+}
