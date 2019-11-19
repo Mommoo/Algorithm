cc9cecab16039c3ea98e51378b73c18671d0ec84
Mommoo <dv.Mommoo@gmail.com>
2019-11-19 22:33:12 +0900

    ## level1: 수박수박수박수박수박수?

---
+++ src/com/mommoo/level2/큰_수_만들기.java
@@ -0,0 +1,14 @@
+package com.mommoo.level2;
+
+public class 큰_수_만들기 {
+    public static void main(String[] args) {
+        System.out.println(new 큰_수_만들기().solution("1924", 2));
+        System.out.println(new 큰_수_만들기().solution("1231234", 3));
+        System.out.println(new 큰_수_만들기().solution("4177252841", 4));
+    }
+
+    public String solution(String number, int k) {
+        String answer = "";
+        return answer;
+    }
+}
