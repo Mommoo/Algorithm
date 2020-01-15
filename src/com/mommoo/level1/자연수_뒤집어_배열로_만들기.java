cc2db2846de5ce3557c0b40801d12c52f772fdc5
Mommoo <dv.Mommoo@gmail.com>
2020-01-15 23:34:13 +0900

    ## level1 자연수 뒤집어 배열로 만들기

---
+++ src/com/mommoo/level1/자연수_뒤집어_배열로_만들기.java
@@ -0,0 +1,19 @@
+package com.mommoo.level1;
+
+import java.util.Arrays;
+import java.util.stream.IntStream;
+
+public class 자연수_뒤집어_배열로_만들기 {
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(new 자연수_뒤집어_배열로_만들기().solution(12345)));
+    }
+
+    public int[] solution(long n) {
+        String num = String.valueOf(n);
+        int len = num.length();
+        return IntStream.range(0, len)
+                        .map(idx -> len - idx - 1)
+                        .map(reverseIdx -> num.charAt(reverseIdx) - '0')
+                        .toArray();
+    }
+}
