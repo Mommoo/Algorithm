01b232793d0852163632f21fe6f0a0d0ca7b0526
Mommoo <dv.Mommoo@gmail.com>
2020-01-31 00:02:57 +0900

    # level2 올바른_괄호

---
+++ src/com/mommoo/level2/올바른_괄호.java
@@ -0,0 +1,27 @@
+package com.mommoo.level2;
+
+public class 올바른_괄호 {
+    public static void main(String[] args) {
+        System.out.println(new 올바른_괄호().solution("()()"));
+        System.out.println(new 올바른_괄호().solution("(())()"));
+        System.out.println(new 올바른_괄호().solution(")()("));
+        System.out.println(new 올바른_괄호().solution("(()("));
+    }
+
+    boolean solution(String s) {
+        int left = 0;
+        for (int idx = 0; idx < s.length(); idx++) {
+            if (s.charAt(idx) == '(') {
+                left++;
+            } else {
+                left--;
+            }
+
+            if (left == -1) {
+                return false;
+            }
+        }
+
+        return left == 0;
+    }
+}
