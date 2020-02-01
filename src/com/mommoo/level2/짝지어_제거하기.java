ed4488c7dafdf105e784c57763539bad70b5d21c
Mommoo <dv.Mommoo@gmail.com>
2020-02-01 23:55:00 +0900

    # level2 짝지어 제거하기

---
+++ src/com/mommoo/level2/짝지어_제거하기.java
@@ -0,0 +1,29 @@
+package com.mommoo.level2;
+
+import java.util.Stack;
+
+public class 짝지어_제거하기 {
+    public static void main(String[] args) {
+        System.out.println(new 짝지어_제거하기().solution("baabaa"));
+        System.out.println(new 짝지어_제거하기().solution("cdcd"));
+    }
+
+    public int solution(String s) {
+        Stack<Character> results = new Stack<>();
+        results.push(s.charAt(0));
+
+        for (int i = 1; i < s.length() ; i++) {
+            char current = s.charAt(i);
+
+            if (!results.isEmpty() && (current == results.peek())) {
+                results.pop();
+                continue;
+            }
+
+            results.push(current);
+        }
+
+
+        return results.isEmpty() ? 1 : 0;
+    }
+}
