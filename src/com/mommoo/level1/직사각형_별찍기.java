c13f401e1efbe1b8bd1ead3d7be7e6551085d967
Mommoo <dv.Mommoo@gmail.com>
2020-02-14 23:12:23 +0900

    # level1 정사각형 별찍기

---
+++ src/com/mommoo/level1/직사각형_별찍기.java
@@ -0,0 +1,25 @@
+package com.mommoo.level1;
+
+import java.util.Scanner;
+
+public class 직사각형_별찍기 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        String line = computeLine(a);
+        while (b-- > 0) {
+            System.out.println(line);
+        }
+    }
+
+    private static String computeLine(int count) {
+        StringBuilder builder = new StringBuilder();
+        while (count-- > 0) {
+            builder.append("*");
+        }
+
+        return builder.toString();
+    }
+}
