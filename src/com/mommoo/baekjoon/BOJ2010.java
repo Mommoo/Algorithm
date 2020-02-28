089355ee46cdff8d05200b63459ab92e8897bd6b
Mommoo <dv.Mommoo@gmail.com>
2020-02-28 22:12:11 +0900

    # 백준: 플러그 (2010)

--- src/com/mommoo/baekjoon/BOJ2010.java
+++ src/com/mommoo/baekjoon/BOJ2010.java
@@ -1,4 +1,20 @@
 package com.mommoo.baekjoon;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 public class BOJ2010 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int count = Integer.parseInt(reader.readLine());
+        int enableSocketCount = 1;
+
+        while (count-- > 0) {
+            int multiTapSocketCount = Integer.parseInt(reader.readLine());
+            enableSocketCount--;
+            enableSocketCount += multiTapSocketCount;
+        }
+
+        System.out.println(enableSocketCount);
+    }
 }
