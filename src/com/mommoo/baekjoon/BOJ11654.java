c9496812d6bde076e1034830dd2e272ca6a4f1ef
Mommoo <dv.Mommoo@gmail.com>
2020-03-06 23:41:03 +0900

    # 백준: 아스키코드 (11654)

---
+++ src/com/mommoo/baekjoon/BOJ11654.java
@@ -0,0 +1,10 @@
+package com.mommoo.baekjoon;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class BOJ11654 {
+    public static void main(String[] args) throws Exception {
+        System.out.println((int)new BufferedReader(new InputStreamReader(System.in)).readLine().charAt(0));
+    }
+}
