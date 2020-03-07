6c87d412e2eb2db27a4471e7b5475f05f301b895
Mommoo <dv.Mommoo@gmail.com>
2020-03-08 01:11:40 +0900

    # 백준: 단어 길이 재기 (2743)

---
+++ src/com/mommoo/baekjoon/BOJ2743.java
@@ -0,0 +1,10 @@
+package com.mommoo.baekjoon;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class BOJ2743 {
+    public static void main(String[] args) throws Exception {
+        System.out.println(new BufferedReader(new InputStreamReader(System.in)).readLine().length());
+    }
+}
