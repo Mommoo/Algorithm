a7c882231c9eab261221b6d499d34c9eb5f59925
Mommoo <dv.Mommoo@gmail.com>
2020-06-24 23:01:45 +0900

    # 백준: 적록색약 (10026): 속도 개선 버전.

--- src/com/mommoo/baekjoon/BOJ10026.java
+++ src/com/mommoo/baekjoon/BOJ10026.java
@@ -3,21 +3,19 @@ package com.mommoo.baekjoon;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.HashSet;
-import java.util.Objects;
-import java.util.Set;
-import java.util.Stack;
-import java.util.function.BiPredicate;
+import java.util.LinkedList;
+import java.util.Queue;
 
 public class BOJ10026 {
+    private static final int[] dx = {1, 0, -1, 0};
+    private static final int[] dy = {0, 1, 0, -1};
     private static char[][] COLOR_MAP;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         COLOR_MAP = createColorMap(br);
 
-        String result = countColorArea(Objects::equals) + " "
-                        + countColorArea((c1, c2) -> isEqualWhenRenAndGreenMode(c1, c2));
+        String result = countColorArea(false) + " " + countColorArea(true);
 
         System.out.println(result);
     }
@@ -39,38 +37,43 @@ public class BOJ10026 {
         return colorMap;
     }
 
-    private static int countColorArea(BiPredicate<Character, Character> biPredicate) {
-        Stack<RowAndCol> rowAndCols = new Stack<>();
+    private static int countColorArea(boolean mode) {
+        Queue<RowAndCol> rowAndCols = new LinkedList<>();
         int count = 0;
-        Set<RowAndCol> needToVisitRowAndCols = createNeedToVisitRowAndCols(COLOR_MAP.length);
-        while (!needToVisitRowAndCols.isEmpty()) {
-            count++;
-            rowAndCols.push(needToVisitRowAndCols.iterator().next());
-            while (!rowAndCols.isEmpty()) {
-                RowAndCol rowAndCol = rowAndCols.pop();
-                needToVisitRowAndCols.remove(rowAndCol);
-                char currentColor = getColor(rowAndCol);
+        int N = COLOR_MAP.length;
+        boolean[][] checks = new boolean[N][N];
 
-                RowAndCol[] nextRowAndCols = rowAndCol.nextRowAndCols();
-                for (RowAndCol next : nextRowAndCols) {
-                    if (needToVisitRowAndCols.contains(next) && biPredicate.test(currentColor, getColor(next))) {
-                        rowAndCols.push(next);
+        for (int row = 0 ; row < N; row++) {
+            for (int col = 0 ; col < N; col++) {
+                if (checks[row][col]) {
+                    continue;
+                }
+                count++;
+
+                rowAndCols.offer(new RowAndCol(row, col));
+                checks[row][col] = true;
+
+                while (!rowAndCols.isEmpty()) {
+                    RowAndCol rowAndCol = rowAndCols.poll();
+                    char currentColor = getColor(rowAndCol);
+                    for (int i = 0; i < 4; i++) {
+                        int nextRow = rowAndCol.row + dy[i];
+                        int nextCol = rowAndCol.col + dx[i];
+                        boolean isInvalid = !(0 <= nextRow && nextRow < N && 0 <= nextCol && nextCol < N);
+
+                        if (isInvalid || checks[nextRow][nextCol] || !(mode ? isEqualWhenRenAndGreenMode(currentColor, COLOR_MAP[nextRow][nextCol]) : currentColor == COLOR_MAP[nextRow][nextCol])) {
+                            continue;
+                        }
+
+                        checks[nextRow][nextCol] = true;
+                        rowAndCols.offer(new RowAndCol(nextRow, nextCol));
                     }
                 }
+
             }
         }
 
         return count;
-    }
-
-    private static Set<RowAndCol> createNeedToVisitRowAndCols(int N) {
-        Set<RowAndCol> rowAndCols = new HashSet<>();
-        for (int row = 0; row < N; row++) {
-            for (int col = 0; col < N; col++) {
-                rowAndCols.add(new RowAndCol(row, col));
-            }
-        }
-        return rowAndCols;
     }
 
     private static char getColor(RowAndCol rowAndCol) {
@@ -78,41 +81,12 @@ public class BOJ10026 {
     }
 
     private static class RowAndCol {
-        private final int[] dx = {1, 0, -1, 0};
-        private final int[] dy = {0, 1, 0, -1};
-
         private final int row;
         private final int col;
 
         public RowAndCol(int row, int col) {
             this.row = row;
             this.col = col;
-        }
-
-        public RowAndCol[] nextRowAndCols() {
-            RowAndCol[] rowAndCols = new RowAndCol[4];
-
-            for (int i = 0; i < 4; i++) {
-                rowAndCols[i] = new RowAndCol(row + dy[i], col + dx[i]);
-            }
-
-            return rowAndCols;
-        }
-
-        @Override
-        public boolean equals(Object o) {
-            if (this == o)
-                return true;
-            if (!(o instanceof RowAndCol))
-                return false;
-            RowAndCol rowAndCol = (RowAndCol) o;
-            return row == rowAndCol.row &&
-                   col == rowAndCol.col;
-        }
-
-        @Override
-        public int hashCode() {
-            return Objects.hash(row, col);
         }
     }
 }
