d4bdb3de8c739499456dc0803632f1513155b136
Mommoo <dv.Mommoo@gmail.com>
2020-08-23 15:57:13 +0900

    # 백준: We love kriii (10718)

---
+++ src/com/mommoo/baekjoon/no10718/Main.java
@@ -0,0 +1,8 @@
+package com.mommoo.baekjoon.no10718;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("강한친구 대한육군\n"
+                           + "강한친구 대한육군");
+    }
+}
