d22326acfb31f9f9827723b75cbe07e2b5d7765e
Mommoo <dv.Mommoo@gmail.com>
2020-09-26 12:11:22 +0900

    # 백준: 니모를 찾아서 (10173)

--- src/com/mommoo/baekjoon/no10173/Main.java
+++ src/com/mommoo/baekjoon/no10173/Main.java
@@ -1,4 +1,38 @@
 package com.mommoo.baekjoon.no10173;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Main {
+    private static final StringBuilder BUILDER = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        while (true) {
+            String line = reader.readLine();
+            if ("EOI".equals(line)) {
+                break;
+            }
+
+            BUILDER.append(containNemoIgnoreCase(line) ? "Found" : "Missing")
+                   .append('\n');
+        }
+
+        System.out.println(BUILDER);
+    }
+
+    private static boolean containNemoIgnoreCase(String line) {
+        for (int i = 0 ; i < line.length(); i++) {
+            char c = Character.toLowerCase(line.charAt(i));
+            if (i <= line.length() - 4 && c == 'n'
+                && Character.toLowerCase(line.charAt(i + 1)) == 'e'
+                && Character.toLowerCase(line.charAt(i + 2)) == 'm'
+                && Character.toLowerCase(line.charAt(i + 3)) == 'o') {
+                return true;
+            }
+        }
+
+        return false;
+    }
 }
